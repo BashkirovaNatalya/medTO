31d10728f09b1dc012c44936785ea0e083a12140
BashkirovaNatalya <67271896+BashkirovaNatalya@users.noreply.github.com>
2022-05-06 00:22:45 +0500

    created category/index init

---
+++ medTO_MVC/medTO_MVC/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace medTO_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
