44d99e0a0dfc5928f5168fad1953ff74a6e1ec5d
BashkirovaNatalya <67271896+BashkirovaNatalya@users.noreply.github.com>
2022-05-06 00:27:16 +0500

    dbcontext for category set

--- medTO_MVC/medTO_MVC/Controllers/CategoryController.cs
+++ medTO_MVC/medTO_MVC/Controllers/CategoryController.cs
@@ -1,11 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using medTO_MVC.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace medTO_MVC.Controllers
 {
     public class CategoryController : Controller
     {
 
-       
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
 
         public IActionResult Index()
         {
